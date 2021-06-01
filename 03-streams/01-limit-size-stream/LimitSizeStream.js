a14490dc589be30a853f22942659b0fc61844854
kla <kla@klas-MacBook-Pro.local>
2021-06-01 10:53:04 +0300

    task 2/1

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -2,13 +2,31 @@ const stream = require('stream');
 const LimitExceededError = require('./LimitExceededError');
 
 class LimitSizeStream extends stream.Transform {
+  #total = 0;
+  limit = 0;
   constructor(options) {
+
     super(options);
+    this.limit = options.limit;
   }
 
   _transform(chunk, encoding, callback) {
+    let error;
+    let data;
+    try{
+      this.#total += chunk.length;
+      if(this.#total > this.limit){
+        throw new LimitExceededError();
+      }
+      data = chunk.toString()
+    }  catch (e) {
+    error = e;
+  }
+  callback(error, data);
+
 
   }
+
 }
 
 module.exports = LimitSizeStream;
